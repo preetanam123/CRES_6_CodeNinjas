168a31f662f9a52ccba8810f76d6fc3aa203b456
janvimehta25 <janvimehta103@gmail.com>
2023-03-11 17:01:39 +0530

    login n signup

--- frontend/src/Pages/Authentication/Login.js
+++ frontend/src/Pages/Authentication/Login.js
@@ -1,9 +1,7 @@
 import React from "react";
-import bg from "../../assets/bg.png";
-// import "./Login.css";
-import login from "../../assets/login.jpeg"
 import verify from "../../assets/verify.svg"
-import google from "../../assets/google.jpg"
+import google from "../../assets/google.png"
+import facebook from "../../assets/facebook.png"
 import axios from "axios";
 import { useNavigate} from "react-router-dom";
 import { useState } from "react";
@@ -47,33 +45,44 @@ const Login = () => {
 
   return (
     <>
-      <div className="w-full h-fit bg-cover bg-[#090E40] min-h-screen min-w-screen">
-        <h1 className="text-6xl font-bold text-white px-16 pt-10">FaceTrustAI</h1>
+      <div className="w-full h-fit bg-cover bg-[#A8DADC] min-h-screen min-w-screen">
+        <h1 className="text-6xl font-bold text-white px-16 pt-10">E-LEARNING</h1>
         <br />
-        <h6 className="text-xl font-semibold text-white px-16">A platform to Verify your documents and safeguard them</h6>
+        {/* <h6 className="text-xl font-semibold text-white px-16">A platform to Verify your documents and safeguard them</h6> */}
         <div className="px-16 py-16 bg-white mx-40 mt-6 mb-6 h-[100%] rounded-3xl flex justify-center" >
           <div className=" w-1/2 h-4/5">
-            <div className="font-bold  ml-4 text-4xl text-center pb-6">Login 
+            <div className="font-bold  ml-4 text-4xl text-center pb-6">LOGIN 
             </div> 
-            <div className="flex justify-center mt-8">
-              <button className="flex justify-evenly items-center text-center w-2/3 h-12 ml-8 bg-gray-200 border-4 border-[#4051A3] rounded-full py-1 font-bold text-lg cursor-pointer">
-                <img src={google} className="h-3/4 rounded-full" /> Log In With Your Google Account
-              </button>
-            </div>
+            
             <div className="font-bold text-[#090E40] text-2xl ml-32 mt-4" >
               Email
             </div>
-            <div className="w-2/3 h-12 bg-gray-200 rounded-full ml-32 mt-4 border-4 border-[#4051A3]">
+            <div className="w-2/3 h-12 bg-gray-200 rounded-xl ml-32 mt-4 drop-shadow-md">
               <input type=" text" className="flex-grow w-3/4 outline-none ml-2 mt-1  bg-transparent h-8 text-lg font-semibold" onChange={handleEmailChange}></input>
             </div>
             <div className="font-bold text-[#090E40] text-2xl ml-32 mt-4">
               Password
             </div>
-            <div className="w-2/3 h-12 bg-gray-200 rounded-full ml-32 mt-4 border-4 border-[#4051A3]">
+            <div className="w-2/3 h-12 bg-gray-200 rounded-xl ml-32 mt-4 drop-shadow-md">
               <input type="password" className="flex-grow w-3/4 outline-none ml-2 mt-1  bg-transparent h-8 text-lg font-semibold" onChange={handlePasswordChange}></input>
             </div>
-            <div className=" ml-52 mt-10 w-32 h-12 ">
-            <button className="bg-[#4051A3] w-64 h-12 rounded-full text-lg  text-white font-extrabold" onClick={handleSubmit}>Submit</button>
+            <div className=" ml-36 mt-10 w-32 h-12 ">
+            <button className="bg-[#131313] w-96 h-12 rounded-xl text-lg  text-white font-extrabold hover:drop-shadow-xl" onClick={handleSubmit}>
+              Submit
+            </button>
+          </div>
+          <div className="text-[#475569] m-8 text-lg text-center">
+              OR
+          </div>
+          <div className="flex justify-center mt-8">
+              <button className="flex justify-evenly items-center text-center w-2/3 h-12 ml-8 bg-[#3A5999] text-white rounded-xl py-1 font-bold text-lg cursor-pointer">
+                <img src={google} className="h-3/4 rounded-full" /> Log In With Your Google Account
+              </button>
+          </div>
+          <div className="flex justify-center mt-8">
+              <button className="flex justify-evenly items-center text-center w-2/3 h-12 ml-8 bg-[#F44236] text-white rounded-xl py-1 font-bold text-lg cursor-pointer">
+                <img src={facebook} className="h-3/4 rounded-full" /> Log In With Your Facebook Account
+              </button>
           </div>
           <div className="font-bold ml-56 mt-4 text-lg flex">
                 New User?<a className=" text-[#4051A3] hover:text-lightgreen" href="/signup">
--- frontend/src/Pages/Authentication/SignUp.js
+++ frontend/src/Pages/Authentication/SignUp.js
@@ -1,5 +1,7 @@
 import React from "react";
 import verify3 from "../../assets/verify3.svg";
+import google from "../../assets/google.png"
+import facebook from "../../assets/facebook.png"
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
@@ -54,25 +56,25 @@ const SignUp = () => {
   };
 
   return (
-    <div className="w-full h-full bg-cover bg-[#090E40] min-h-[100%] min-w-screen">
-      <h1 className="text-6xl font-bold text-white px-16 pt-10">FaceTrustAI</h1>
+    <div className="w-full h-full bg-cover bg-[#FFACAC] min-h-[100%] min-w-screen">
+      <h1 className="text-6xl font-bold text-white px-16 pt-10">E-LEARNING</h1>
       <br />
-      <h6 className="text-xl font-semibold text-white px-16">
+      {/* <h6 className="text-xl font-semibold text-white px-16">
         SignUp with us and secure your documents by verifying
-      </h6>
+      </h6> */}
       <div className="px-16 py-16 bg-white mx-40 mt-6 mb-6 h-[100%] rounded-3xl flex justify-center">
         <div className="w-1/2 h-4/5 mt-10">
           <img src={verify3} className="h-full w-4/5 ml-14" alt="" />
         </div>
         <div className=" w-1/2 h-4/5">
-          <div className="font-bold  ml-4 text-4xl text-center pb-6">
-            SignUp
+          <div className="font-bold  ml-4 text-3xl text-center pb-6">
+            CREATE ACCOUNT
           </div>
           <div className="justify-center mt-8">
             <div className="font-bold text-[#090E40] text-2xl ml-32 mt-4">
               Name
             </div>
-            <div className="w-2/3 h-12 bg-gray-200 rounded-full ml-32 mt-4 border-4 border-[#4051A3]">
+            <div className="w-2/3 h-10 bg-gray-200 rounded-xl ml-32 mt-4 drop-shadow-md">
               <input
                 type=" text"
                 className="flex-grow w-3/4 outline-none ml-2 mt-1  bg-transparent h-8 text-lg font-semibold"
@@ -84,7 +86,7 @@ const SignUp = () => {
           <div className="font-bold text-[#090E40] text-2xl ml-32 mt-4">
             Email
           </div>
-          <div className="w-2/3 h-12 bg-gray-200 rounded-full ml-32 mt-4 border-4 border-[#4051A3]">
+          <div className="w-2/3 h-10 bg-gray-200 drop-shadow-md rounded-xl ml-32 mt-4">
             <input
               type=" text"
               className="flex-grow w-3/4 outline-none ml-2 mt-1  bg-transparent h-8 text-lg font-semibold"
@@ -95,7 +97,7 @@ const SignUp = () => {
           <div className="font-bold text-[#090E40] text-2xl ml-32 mt-4">
             Password
           </div>
-          <div className="w-2/3 h-12 bg-gray-200 rounded-full ml-32 mt-4 border-4 border-[#4051A3]">
+          <div className="w-2/3 h-10 bg-gray-200 rounded-xl ml-32 mt-4 drop-shadow-md ">
             <input
               type="password"
               className="flex-grow w-3/4 outline-none ml-2 mt-1  bg-transparent h-8 text-lg font-semibold"
@@ -103,15 +105,28 @@ const SignUp = () => {
               value={password}
             ></input>
           </div>
-          <div className=" ml-52 mt-10 w-32 h-12 ">
+          <div className="ml-36 mt-10 w-32 h-12 ">
             <button
-              className="bg-[#4051A3] w-64 h-12 rounded-full text-lg  text-white font-extrabold"
+              className="bg-[#131313] w-96 h-12 rounded-xl text-lg  text-white font-extrabold hover:drop-shadow-xl"
               onClick={handleSubmit}
             >
               Submit
             </button>
           </div>
-          <div className="font-bold ml-56 mt-4 text-lg flex">
+          <div className="text-[#475569] m-10 text-lg text-center">
+              OR
+          </div>
+          <div className="flex justify-center mt-8">
+              <button className="flex justify-evenly items-center text-center w-2/3 h-12 ml-8 bg-[#3A5999] text-white rounded-xl py-1 font-bold text-lg cursor-pointer">
+                <img src={google} className="h-3/4 rounded-full" /> Log In With Your Google Account
+              </button>
+          </div>
+          <div className="flex justify-center mt-8">
+              <button className="flex justify-evenly items-center text-center w-2/3 h-12 ml-8 bg-[#F44236] text-white rounded-xl py-1 font-bold text-lg cursor-pointer">
+                <img src={facebook} className="h-3/4 rounded-full" /> Log In With Your Facebook Account
+              </button>
+          </div>
+          <div className="font-bold ml-56 mt-10 text-lg flex">
             Already a User?&nbsp;
             <a className=" text-[#4051A3] hover:text-lightgreen" href="/login">
               Login
