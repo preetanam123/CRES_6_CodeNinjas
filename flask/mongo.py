5c71cc94a9237113a8c3ce11a5cc80d280f00673
preetanam123 <80145584+preetanam123@users.noreply.github.com>
2023-03-11 23:45:08 +0530

    flask and symptoms

--- flask/mongo.py
+++ flask/mongo.py
@@ -3,3 +3,15 @@ import pandas as pd
 from bson import json_util
 import json 
 
+def getDisease():
+    client = pymongo.MongoClient(
+    'mongodb+srv://Preet:Preet1$A@cluster0.xpb6hhy.mongodb.net/?retryWrites=true&w=majority')
+    db = client['test']
+    collections = db['symptoms']
+    allDocs = collections.find()
+    allDocs = json.loads(json_util.dumps(allDocs))
+    lst = list(allDocs)
+    df = pd.DataFrame(lst)
+    print(df)
+
+getDisease()
