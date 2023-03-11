b0e253fe42155d848f15fef06ef83296837c2f6a
preetanam123 <80145584+preetanam123@users.noreply.github.com>
2023-03-12 00:15:40 +0530

    symptoms node backend

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,13 @@ const cors = require("cors");
 const app = express();
 const userRoutes = require("./routes/user.route");
 const personalRoutes = require("./routes/personal.route");
+const symptomsRoutes = require("./routes/symptoms.route");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", userRoutes);
 app.use("/api/personal", personalRoutes);
+app.use("/api/symptoms", symptomsRoutes);
 
 module.exports = app;
